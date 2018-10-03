392fb5d3be693e41e292e8e7e694cd64dee54d11
Henry Zhao <henry.zhao19931004@gmail.com>
2018-10-03 17:49:49 -0400

    update tcp

--- Template/Server/run_tcp_server.sh
+++ Template/Server/run_tcp_server.sh
@@ -1,4 +1,4 @@
 #Usage: ./run_server.sh [<tcp_server_name>]
 
 ./run_rmi.sh > /dev/null 2>&1
-java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$(pwd)/ Server.RMI.TCPResourceManager $1
+java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$(pwd)/ Server.TCP.TCPResourceManager $1
