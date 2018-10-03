263db7caac463e9e4e7653ada55c64990537d4eb
bilasa <43503082+bilasa@users.noreply.github.com>
2018-10-02 23:03:16 -0400

    fix run servers script

--- Template/Server/run_servers.sh
+++ Template/Server/run_servers.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 
 #TODO: SPECIFY THE HOSTNAMES OF 4 CS MACHINES (lab1-1, cs-2, etc...)
-MACHINES=("cs-1.cs.mcgill.ca", "cs-2.cs.mcgill.ca", "cs-3.cs.mcgill.ca", "cs-4.cs.mcgill.ca", "cs-5.cs.mcgill.ca")
+MACHINES=(cs-4.cs.mcgill.ca cs-5.cs.mcgill.ca cs-6.cs.mcgill.ca cs-7.cs.mcgill.ca cs-8.cs.mcgill.ca)
 
 tmux new-session \; \
 	split-window -h \; \
+	split-window -v \; \
 	split-window -v \; \
 	split-window -v \; \
 	select-layout main-vertical \; \
@@ -14,7 +15,7 @@ tmux new-session \; \
 	send-keys "ssh -t ${MACHINES[1]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Cars\"" C-m \; \
 	select-pane -t 4 \; \
 	send-keys "ssh -t ${MACHINES[2]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Rooms\"" C-m \; \
-    select-pane -t 5 \; \
-    send-keys "ssh -t ${MACHINES[3]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Customers\"" C-m \; \
+    	select-pane -t 5 \; \
+    	send-keys "ssh -t ${MACHINES[3]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Customers\"" C-m \; \
 	select-pane -t 1 \; \
 	send-keys "ssh -t ${MACHINES[4]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; sleep .5s; ./run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]} ${MACHINES[3]}\"" C-m \;
