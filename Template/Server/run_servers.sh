214265d03a76c611087870ef41ada5b9b7396c53
Henry Zhao <henry.zhao19931004@gmail.com>
2018-10-03 14:23:01 -0400

    tmux

--- Template/Server/run_servers.sh
+++ Template/Server/run_servers.sh
@@ -15,7 +15,7 @@ tmux new-session \; \
 	send-keys "ssh -t ${MACHINES[1]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Cars\"" C-m \; \
 	select-pane -t 4 \; \
 	send-keys "ssh -t ${MACHINES[2]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Rooms\"" C-m \; \
-	select-pane -t 5 \; \
-    send-keys "ssh -t ${MACHINES[3]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Customers\"" C-m \; \
 	select-pane -t 1 \; \
+    send-keys "ssh -t ${MACHINES[3]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; ./run_server.sh Customers\"" C-m \; \
+	select-pane -t 0 \; \
 	send-keys "ssh -t ${MACHINES[4]} \"cd $(pwd) > /dev/null; echo -n 'Connected to '; hostname; sleep .5s; ./run_middleware.sh ${MACHINES[0]} ${MACHINES[1]} ${MACHINES[2]} ${MACHINES[3]}\"" C-m \;
