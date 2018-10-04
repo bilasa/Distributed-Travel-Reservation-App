131d4e2940d1f2c6d7e1cdad2ba8c99c115346ac
Henry Zhao <henry.zhao19931004@gmail.com>
2018-10-03 23:18:30 -0400

    scripts

--- Template/Server/run_tcp_server.sh
+++ Template/Server/run_tcp_server.sh
@@ -1,4 +1,4 @@
 #Usage: ./run_server.sh [<tcp_server_name>]
 
 ./run_rmi.sh > /dev/null 2>&1
-java -Djava.security.policy=java.policy Server.TCP.TCPResourceManager $1
+java Server.TCP.TCPResourceManager $1
